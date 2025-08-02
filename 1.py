43256a6cbd687b9564e34c0bf6ca66dec5da8713
exhilarating0411 <94095673+exhilarating0411@users.noreply.github.com>
2025-08-02 21:36:58 +0800

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,1 +1,9 @@
-print("hello")
+import streamlit as st
+
+st.title("小型计算应用")
+name = st.text_input("输入你的名字：")
+num = st.number_input("输入一个数字：", value=0)
+
+if st.button("计算平方"):
+    st.write(f"{name}，你的数字平方是：{num ** 2}")
+
